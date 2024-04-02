8042f6790f3338f3043ccd8fd463d1488d5c17ed
xfy <i@rua.plus>
2024-04-02 14:29:01 +0800

    update group anagrams v2

--- src/group-anagrams/group-anagrams-v2.rs
+++ src/group-anagrams/group-anagrams-v2.rs
@@ -1,51 +1,24 @@
 use std::{collections::HashMap, vec};
 
+fn hash(str: &str) -> Vec<u8> {
+    let mut res = str.chars().fold(vec![], |mut prev, cur| {
+        let mut bytes = [0u8; 1];
+        cur.encode_utf8(&mut bytes);
+        prev.push(bytes[0]);
+        prev
+    });
+    res.sort();
+    res
+}
+
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let primes = HashMap::from([
-            ('a', 2),
-            ('b', 3),
-            ('c', 5),
-            ('d', 7),
-            ('e', 11),
-            ('f', 13),
-            ('g', 17),
-            ('h', 19),
-            ('i', 23),
-            ('j', 29),
-            ('k', 31),
-            ('l', 37),
-            ('m', 41),
-            ('n', 43),
-            ('o', 47),
-            ('p', 53),
-            ('q', 59),
-            ('r', 61),
-            ('s', 67),
-            ('t', 71),
-            ('u', 73),
-            ('v', 79),
-            ('w', 83),
-            ('x', 89),
-            ('y', 97),
-            ('z', 101),
-        ]);
-
         let mut result = HashMap::new();
         for str in strs {
-            let mut x = 1;
-            for s in str.chars() {
-                x *= primes.get(&s).expect("cannot find target alphabet");
-                // println!("{}", x);
-            }
-            let mut res_arr = result.entry(x).or_insert(vec![]);
+            let mut key = hash(&str);
+            let mut res_arr = result.entry(key).or_insert(vec![]);
             res_arr.push(str);
-            // println!("====================");
         }
-        // println!("{:?}", result);
-        result
-            .into_values()
-            .into_iter()
-            .collect::<Vec<Vec<String>>>()
+        result.into_values().into_iter().collect()
     }
 }
